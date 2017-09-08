40b742469ee1bfedef9c44ae9c359672eb380cf3
braamconradie <braam.conradie@gmail.com>
2017-09-09 08:46:40 +1200

    Update app.py
    commented out more stuff

--- app.py
+++ app.py
@@ -52,6 +52,7 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "braameksp":
         return {}
+   """
     baseurl = "https://query.yahooapis.com/v1/public/yql?"
     yql_query = makeYqlQuery(req)
     if yql_query is None:
@@ -60,6 +61,8 @@ def processRequest(req):
     result = urlopen(yql_url).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
+    """
+
     try:
         result = urlopen('https://dweet.io/dweet/for/braamapiai?hello=wennerz').read()
     except:
